413448db48e10da3fab37166b80a379162a8aa91
AnosovAnton <spector.rebop@mail.ru>
2020-08-07 02:29:11 +0300

    урок2

---
+++ ClientVK/ClientVK/MyCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyCollectionViewCell.swift
+//  ClientVK
+//
+//  Created by spector.rebop on 07.08.2020.
+//  Copyright © 2020 spector.rebop. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+     @IBOutlet weak var myPicture: UIImageView!
+    
+    
+    
+}
